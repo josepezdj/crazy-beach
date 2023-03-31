4310836502ca1f75116bc3c71e74ed3a5c5a3979
josepezdj <josepezdj@hotmail.es>
2023-03-31 17:58:40 +0200

    [crazy-beach][jpdj][004] - Styled and added content to Header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,65 @@
 import { LitElement, html } from 'lit';
 import styles from './header.scss';
+import { playerService } from '../../services/player-service';
+import 'fa-icons';
+import { CRAZY_BEACH } from '../../data/constants';
 
 export class Header extends LitElement {
-    static get styles() {
-        return styles;
+    static get properties() {
+        return {
+            currentPlayer: { type: Object },
+        };
+    }
+
+    constructor() {
+        super();
+    }
+
+    firstUpdated() {
+        this.currentPlayer = playerService.getCurrentPlayer();
     }
 
     render() {
-        return html`<h2>Header</h2>`;
+        return html`
+            <div class="header">
+                <nav class="header__nav">
+                    <a class="header__goback" href="/">
+                        <fa-icon class="fas fa-arrow-circle-left" size="1.7em"></fa-icon>
+                    </a>
+                    <div class="header__icon-app">
+                        <div class="header__icon-text">
+                            <span>${CRAZY_BEACH.MAIN_APP.TITLE1}</span>
+                        </div>
+                        <fa-icon class="fas fa-umbrella-beach" size="2em"></fa-icon>
+                        <div class="header__icon-text">
+                            <span>${CRAZY_BEACH.MAIN_APP.TITLE2}</span>
+                        </div>
+                    </div>
+                    <ul class="header__list">
+                        <!-- <li class="header__item">
+                            <a class="header__link" href="#">
+                                <fa-icon class="fas fa-scroll header__account--link" size="1.5em"></fa-icon>
+                            </a>
+                        </li>
+                        <li class="header__item">
+                            <a class="header__link" href="#">
+                                <fa-icon class="fas fa-medal header__account--link" size="1.5em"></fa-icon>
+                            </a>
+                        </li> -->
+                        <li class="header__item">
+                            <a class="header__account" href="#">
+                                <fa-icon class="fas fa-user-circle header__account--icon"></fa-icon>
+                                <span class="header__account--name">${this.currentPlayer?.name}</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        `;
+    }
+
+    static get styles() {
+        return styles;
     }
 }
 
