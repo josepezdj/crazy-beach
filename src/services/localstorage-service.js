85e29f44bc59f36bfcf79ae692dc05fac9b78638
josepezdj <josepezdj@hotmail.es>
2023-03-31 17:56:21 +0200

    [crazy-beach][jpdj][003] - Created localstorageService and playerService, and main styles variables

---
+++ src/services/localstorage-service.js
@@ -0,0 +1,27 @@
+export const localstorageService = (() => {
+	const services = {}
+    services.createNewEmptyState  = () => {
+        return {
+            currentPlayer: '',
+            players: [],
+        };
+	}
+	services.saveStateToLocalStorage  = (state) => {
+        window.localStorage.setItem('state', JSON.stringify(state));
+    }
+    services.getStateFromLocalStorage = () => {
+        const state = window.localStorage.getItem('state');
+        if (state !== undefined && state !== null) return JSON.parse(state);
+        return -1;
+    }
+    services.findPlayerInStatePlayers = (name) => {
+        let state = window.localStorage.getItem('state');
+        if (state !== undefined && state !== null) {
+            state = JSON.parse(state);
+            const foundUser = state.players.find(player => player.name === name);
+            if (foundUser) return foundUser;
+        }
+        return -1;
+    }
+	return services;
+})();
---
+++ src/services/player-service.js
@@ -0,0 +1,69 @@
+import { localstorageService } from './localstorage-service';
+import { generateRandomId } from '../utils/generate-random-id';
+
+export const playerService = (() => {
+	const services = {}
+    services.createNewPlayer = (name) => {
+        return {
+            id: generateRandomId(),
+            name,
+            maxPoints: 0,
+            currentPoints: 0,
+            avatar: '',
+            collectedObjects: [],
+        };        
+    }
+    services.setCurrentPlayer = (player) => {
+        let state = localstorageService.getStateFromLocalStorage();
+        if (state !== -1) {
+            state.currentPlayer = player;
+            localstorageService.saveStateToLocalStorage(state);
+        }
+    }
+    services.getCurrentPlayer = () => {
+        const state = localstorageService.getStateFromLocalStorage();
+        if (state !== -1 && state.currentPlayer !== '') return state.currentPlayer;
+        return -1;
+    }
+    services.getAllPlayers = () => {
+        const state = localstorageService.getStateFromLocalStorage();
+        if (state !== -1 && state.players.length > 0) {
+            return state.players.sort((a, b) => b.maxPoints - a.maxPoints);
+        }
+        return -1;
+    }
+    services.updateCurrentPlayer = (prop, value) => {
+        let state = localstorageService.getStateFromLocalStorage();
+        let currentPlayer;
+        if (state !== -1 && state.currentPlayer !== '') {
+            currentPlayer = state.currentPlayer;
+            if (!currentPlayer[prop] === value) {
+                // Update currentPlayer
+                state.currentPlayer[prop] = value;
+                // Update player in players list
+                const foundPlayer = localstorageService.findPlayerInStatePlayers(state.currentPlayer.name)
+                foundPlayer[prop] = value;
+                const playerIndex = state.players.map(player => player.name).indexOf(foundPlayer.name);
+                state.players.splice(playerIndex, 1, foundPlayer);
+                // Save new state
+                localstorageService.saveStateToLocalStorage(state);
+                // Return player modified
+                return state.currentPlayer;
+            }
+        }
+        return -1;
+    }
+    services.deleteCurrentPlayer = () => {
+        let state = localstorageService.getStateFromLocalStorage();
+        if (state !== -1 && state.currentPlayer !== '') {
+            // Delete player from players list
+            state = state.players.filter(player => player.name !== state.currentPlayer.name);
+            // Unset currentPlayer
+            state.curentPlayer = '';
+            // Save state
+            localstorageService.saveStateToLocalStorage(state);
+        }
+    }
+
+return services;
+})();
